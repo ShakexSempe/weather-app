33cc86ee4ec40c96b49aa89e6f2f7471f3d9cbfb
Ray-Shakex <bshakex@gmail.com>
2023-08-28 11:30:43 +0200

    weather.js

---
+++ weather.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+// https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+    return axios.get("https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime", {
+        params: {
+            latitude: lat,
+            longitude: lon,
+            timezone,
+        }
+    }).then(({ data }) => {
+        return {
+            current: parseCurrentWeather(data) ,
+            daily: parseDailyWeather(data),
+            hourly: parseHourlyWeather(data) 
+        }
+    })
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+    const { temperature: currentTemp, windspeed: windSpeed, weathercode: iconCode } = current_weather
+
+    // return {
+    //     currentTemp ,
+    //     highTemp: ,
+    //     lowTemp: ,
+    //     hightFeelsLike: ,
+    //     lowFeelsLike: ,
+    //     windSpeed ,
+    //     precip: ,
+    //     iconCode ,
+    // }
+}
