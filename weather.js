a0035d70652ab67b2f3cf16f696c70fe30668e89
Ray-Shakex <bshakex@gmail.com>
2023-08-30 11:43:12 +0200

    parseCurrentWeather precip_sum*

--- weather.js
+++ weather.js
@@ -1,29 +1,27 @@
 import axios from 'axios'
 
-// https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime
-
 export function getWeather(lat, lon, timezone) {
     return axios.get("https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime", {
         params: {
             latitude: lat,
             longitude: lon,
             timezone,
+        },
+    })
+    .then(({ data }) => {
+        return {
+            current: parseCurrentWeather(data),
+            // daily: parseDailyWeather(data),
+            // hourly: parseHourlyWeather(data) 
         }
     })
-    // .then(({ data }) => {
-    //     return {
-    //         current: parseCurrentWeather(data) ,
-    //         daily: parseDailyWeather(data),
-    //         hourly: parseHourlyWeather(data) 
-    //     }
-    // })
 }
 
 function parseCurrentWeather({ current_weather, daily }) {
     const {
         temperature: currentTemp,
         windspeed: windSpeed,
-        weathercode: iconCode
+        weathercode: iconCode,
     } = current_weather;
 
     const {
@@ -31,19 +29,17 @@ function parseCurrentWeather({ current_weather, daily }) {
         temperature_2m_min: [minTemp],
         apparent_temperature_max: [maxFeelsLikeTemp],
         apparent_temperature_min: [minFeelsLikeTemp],
-        precipitation: [precip],
+        precipitation_sum: [precip],
     } = daily
 
-    
-
     return {
-        currentTemp,
-        highTemp: maxTemp,
-        lowTemp: minTemp,
-        hightFeelsLike: maxFeelsLikeTemp,
-        lowFeelsLike: minFeelsLikeTemp,
-        windSpeed,
-        precip: precip,
+        currentTemp: Math.round(currentTemp),
+        highTemp: Math.round(maxTemp),
+        lowTemp: Math.round(minTemp),
+        hightFeelsLike: Math.round(maxFeelsLikeTemp),
+        lowFeelsLike: Math.round(minFeelsLikeTemp),
+        windSpeed: Math.round(windSpeed),
+        precip: Math.round(precip * 100) / 100,
         iconCode,
     }
 }
