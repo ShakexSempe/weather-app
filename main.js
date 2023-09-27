55936176f0827b1828a298c253a58a80adcca64f
Ray-Shakex <bshakex@gmail.com>
2023-09-27 10:39:43 +0200

    renderWeather

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 import './css/style.css'
 import { getWeather } from './weather'
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    data => {
-    console.log(data);
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(renderWeather)
+.catch(e => {
+    console.error(e)
+    alert("Error getting weather")
 })
+
+
+function renderWeather({ current, daily, hourly }) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove('blurred')
+}
+
+// HELPER FUNCTION
+
+
+function renderCurrentWeather(current) {
+    document.querySelector("[data-current-temp]").textContent = 
+    current.currentTemp
+}
