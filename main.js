194b89a0857168af335d4e0a1d2c7972459f7ce5
Ray-Shakex <bshakex@gmail.com>
2023-10-09 13:53:36 +0200

    iconMap

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './css/style.css'
 import { getWeather } from './weather'
+import { ICON_MAP } from './iconMap'
 
 getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
 .then(renderWeather)
@@ -21,12 +22,16 @@ function setValue(selector, value,{ parent = document } = {} ) {
     parent.querySelector(`[data-${selector}]`).textContent = value
 }
 
+// ICONS URL FUNCTION
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
 
 // CURRENT WEATHER ICON
 const currentIcon = document.querySelector("[data-current-icon]")
 // RENDER CURRENT WEATHER 
 function renderCurrentWeather(current) {
-    // currentIcon.src = getIconUrl(current.iconCode);
+    currentIcon.src = getIconUrl(current.iconCode);
     setValue("current-temp", current.currentTemp);
     setValue("current-high", current.highTemp);
     setValue("current-low", current.lowTemp);
